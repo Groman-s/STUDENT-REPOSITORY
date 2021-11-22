ccc1d36d4a8f6ca516701476be90a644a734a376
Groman-s <goyanov99@mail.ru>
2021-11-22 18:09:54 +0300

    Добавил отчёт по ЛР 4.

---
+++ JavaLabs/src/com/goyanov/Main.java
@@ -0,0 +1,18 @@
+package com.goyanov;
+
+import javax.swing.*;
+import javax.swing.table.TableModel;
+import javax.swing.tree.TreeNode;
+import java.awt.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        TableModel k;
+        k.set
+    }
+}
